b35eec26a53e0c0cc6d37218ded52042a9521319
Thomas Carmichael <ThomasCarmichael5@gmail.com>
2020-07-04 11:53:04 -0500

    Initial commit -- Add game interface and starting menu screen options.

---
+++ Assets/Game.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+  public void GameToMenu ()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+  }
+}
